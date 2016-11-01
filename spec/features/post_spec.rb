da61e8dccf5cc94470c86e68a6b266bc114a7816
Silver <silver.mccall@yahoo.com>
2016-11-01 12:17:13 -0600

    Implemented new index query for showing users posts they created and hiding others

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -25,6 +25,18 @@ describe 'navigate' do
       visit posts_path
       expect(page).to have_content(/Rationale|content/)
     end
+
+    it 'has a scope so that only post creators can see their posts' do
+      post1 = Post.create(date: Date.today, rationale: "asdf", user_id: @user.id)
+      post2 = Post.create(date: Date.today, rationale: "asdf", user_id: @user.id)
+
+      other_user = User.create(first_name: 'Non', last_name: 'Authorized', email: "nonauth@example.com", password: "asdfasdf", password_confirmation: "asdfasdf")
+      post_from_other_user = Post.create(date: Date.today, rationale: "This post shouldn't be seen", user_id: other_user.id)
+
+      visit posts_path
+
+      expect(page).to_not have_content(/This post shouldn't be seen/)
+    end
   end
 
   describe 'new' do
@@ -35,16 +47,18 @@ describe 'navigate' do
       expect(page.status_code).to eq(200)
     end
   end
+  
+describe 'delete' do
+  it 'can be deleted' do
+    @post = FactoryGirl.create(:post)
 
-  describe 'delete' do
-    it 'can be deleted' do
-      @post = FactoryGirl.create(:post)
-      visit posts_path
+    @post.update(user_id: @user.id)
+    visit posts_path
 
-      click_link("delete_post_#{@post.id}_from_index")
-      expect(page.status_code).to eq(200)
-    end
+    click_link("delete_post_#{@post.id}_from_index")
+    expect(page.status_code).to eq(200)
   end
+end
 
   describe 'creation' do
     before do
