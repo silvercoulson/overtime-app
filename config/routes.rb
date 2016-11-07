e7cf9afa6fad4db955c93ef9bcf058ab3cc11cc9
Silver <silver.mccall@yahoo.com>
2016-11-07 14:14:37 -0700

    Implemented initial view code for audit logs

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  resources :audit_logs
-  namespace :admin do
-    resources :users
-resources :posts
-resources :admin_users
+	resources :audit_logs, except: [:new, :edit, :destroy]
+  
+  	namespace :admin do
+    	resources :users
+		resources :posts
+		resources :admin_users
 
-    root to: "users#index"
+    	root to: "users#index"
   end
 
   resources :posts
---
+++ spec/features/audit_log_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe 'AuditLog Feature' do
+  let(:audit_log) { FactoryGirl.create(:audit_log) }
+
+describe 'index' do
+  before do
+    admin_user = FactoryGirl.create(:admin_user)
+    login_as(admin_user, :scope => :user)
+  end
+
+	it 'renders audit log content' do
+		FactoryGirl.create(:audit_log)
+		visit audit_logs_path
+		expect(page).to have_content(/SNOW/)
+	end
+  end
+end
+
